ae65675e7edd2c2c3a61b89dcdb1790e0356ecd0
Marco Di Benedetto <marco.dibenedetto@isti.cnr.it>
2023-03-30 19:42:47 +0200

    grumpf

---
+++ train.py
@@ -0,0 +1,36 @@
+from denoising_diffusion_pytorch import Unet3D, GaussianDiffusion3D, Trainer3D
+
+
+def main():
+	model = Unet3D(
+		dim = 64,
+		dim_mults = (1, 2, 4, 8),
+	)
+
+	diffusion = GaussianDiffusion3D(
+		model,
+		image_size = 64,
+		timesteps = 1000,           # number of steps
+		sampling_timesteps = 250,   # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
+		loss_type = 'l1',           # L1 or L2
+	)
+
+	trainer = Trainer3D(
+		diffusion,
+		'C:/devel/mri-anomaly/data/normal',
+		#train_batch_size = 32,
+		train_batch_size = 1,
+		train_lr = 8e-5,
+		#train_num_steps = 700000,         # total training steps
+		train_num_steps = 2,              # total training steps
+		gradient_accumulate_every = 2,    # gradient accumulation steps
+		ema_decay = 0.995,                # exponential moving average decay
+		amp = True,                       # turn on mixed precision
+		calculate_fid = False,            # whether to calculate fid during training
+	)
+
+	trainer.train()
+
+
+if __name__ == '__main__':
+	main()
